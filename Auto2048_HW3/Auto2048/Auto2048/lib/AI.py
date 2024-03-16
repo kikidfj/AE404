4137cbff81d9d9b7ffb4df031dc526c4f73f18da
kikidfj <https://github.com/kikidfj/AE404>
2024-03-16 15:03:15 +0800

    add AI.py

---
+++ Auto2048_HW3/Auto2048/Auto2048/lib/AI.py
@@ -0,0 +1,16 @@
+from lib.Grib import * 
+import asyncio
+import random
+import multiprocessing
+
+class AI:
+	def __init__(self, multi_core=False):
+
+		self.init_dict()
+	def sim_game(self, game_state):
+
+
+	def next_move(self, game_state, n_games):
+
+
+	def init_dict(self):
