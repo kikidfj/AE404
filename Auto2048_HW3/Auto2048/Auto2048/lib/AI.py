67ca5c422a0d70ecaf1fe1a7c665701fcbe9279e
kikidfj <https://github.com/kikidfj/AE404>
2024-05-22 23:48:56 +0800

    2048ai

--- Auto2048_HW3/Auto2048/Auto2048/lib/AI.py
+++ Auto2048_HW3/Auto2048/Auto2048/lib/AI.py
@@ -5,12 +5,30 @@ import multiprocessing
 
 class AI:
 	def __init__(self, multi_core=False):
-
+		self.multi_core = multi_core
 		self.init_dict()
+
 	def sim_game(self, game_state):
-
-
+		temp_game = Grib(template = game_state)
+		first_move = random.choice(list(Moves))
+		temp_game.move(first_move)
+		while True:
+			if not temp_game.move(random.choice(list(Moves))): break
+		return (first_move, temp_game.score())
 	def next_move(self, game_state, n_games):
-
+		results = []
+		for i in range(n_games):
+			results.append(self.sim_game(copy.deepcopy(game_state)))
+		for result in results:
+			self.move_results[result[0]].append(result[1])
+		choices = []
+		for i in Moves:
+			if len(self.move_results[i]) == 0: continue
+			choices.append((i, sum(self.move_results[i]) / len(self.more_results[i])))
+		choices.sort(key=lambda x: x[1], reverse=True)
+		self,init_dict()
+		print(choices[0][0].name)
+		return choices[0][0]
 
 	def init_dict(self):
+		self.move_results = {}
--- monteCarlo/monteCarloPi.py
+++ monteCarlo/monteCarloPi.py
@@ -1,19 +1,35 @@
 import random
+import threading
 
-TOTAL_POINTS = 10000000
+TOTAL_POINTS = 100000
 RADIUS = 10000
 
-xs = []
-ys = []
-pt = 0
+def monteCarloPi(totalPoints):
+	xs = []
+	ys = []
+	pt = 0
 
-for i in range(TOTAL_POINTS):
-	xs.append(random.randint(0,RADIUS))
-	ys.append(random.randint(0,RADIUS))
+	for i in range(totalPoints):
+		xs.append(random.randint(0,RADIUS))
+		ys.append(random.randint(0,RADIUS))
+	print("finish gerate points")
+	for i in range(totalPoints):
+		if(xs[i]*xs[i] + ys[i]*ys[i]) < RADIUS*RADIUS:
+			pt += 1
+	print("finish calculate distance")
+	print("totalpoints: " + str(totalPoints) + ", " +"pt:" + str(pt))
+	return pt
 
-for i in range(TOTAL_POINTS):
-	if(xs[i]*xs[i] + ys[i]*ys[i]) < RADIUS*RADIUS:
-		pt += 1
-monteCarloPi = float(pt)/TOTAL_POINTS * 4.0
 
-print("pi:" + str(monteCarloPi))
+
+def main():
+	p1 = monteCarloPi(int(TOTAL_POINTS/4))
+	p2 = monteCarloPi(int(TOTAL_POINTS/4))
+	p3 = monteCarloPi(int(TOTAL_POINTS/4))
+	p4 = monteCarloPi(int(TOTAL_POINTS/4))
+
+	Pi = 4 * float(p1 + p2 + p3 + p4)/TOTAL_POINTS
+	print("pi:" + str(Pi))
+
+if __name__ == '__main__':
+	main()
